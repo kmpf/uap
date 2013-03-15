63da0ef09939729c504be4396cba1ae64649efc3
Michael Specht <micha.specht@gmail.com>
2013-03-15 08:53:35 +0100

    commented bootstrapping script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+
+# This script creates a virtual Python environment in ./python-env with all required libraries set up.
+# To use it, call Python at ./python-env/bin/python.
+
 virtualenv python_env
 ./python_env/bin/pip install textile pyyaml
-
