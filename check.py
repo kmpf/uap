d8a4c7b4d5e567eb5cd13d6e9f535a6804fe9c38
Michael Specht <micha.specht@gmail.com>
2013-03-15 10:28:51 +0100

    collect sample information

--- check.py
+++ check.py
@@ -1,6 +1,10 @@
 #!./python_env/bin/python
+
+# This script loads the configuration and prints some information about it.
+
 import sys
-sys.path.append('include')
+sys.path.append('./include')
 import pipeline
 
 p = pipeline.Pipeline()
+print >> sys.stderr, p
--- include/pipeline.py
+++ include/pipeline.py
@@ -1,19 +1,31 @@
 import os
+import glob
+import sys
 import yaml
 
-class ConfigurationException(Exception):
-    def __init__(self, value):
-        self.value = value
-    def __str__(self):
-        return repr(self.value)
-    
 class Pipeline:
-    def __init__(self):
-        self.config = {}
-        self.read_config()
+
+    # an exception class for reporting configuration errors
+    class ConfigurationException(Exception):
+        def __init__(self, value):
+            self.value = value
+        def __str__(self):
+            return repr(self.value)
         
+    def __init__(self):
+        
+        # the configuration as read from config.yaml
+        self.config = {}
+        
+        # dictionary of sample names => information
+        self.all_samples = {}
+        
+        self.read_config()
+        self.gather_information()
+        
+    # read configuration and make sure it's good
     def read_config(self):
-        print("Reading configuration...")
+        print >> sys.stderr, "Reading configuration..."
         self.config = yaml.load(open('config.yaml'))
         if not 'sourcePaths' in self.config:
             raise ConfigurationException("Missing key: sourcePaths")
@@ -25,3 +37,22 @@ class Pipeline:
         if not os.path.exists(self.config['destinationPath']):
             raise ConfigurationException("Destination path does not exist: " + self.config['destinationPath'])
         
+    # for every source path, look for samples in [path]/Unaligned/Project_*/Sample_*
+    def gather_information(self):
+        print >> sys.stderr, "Gathering information..."
+        self.all_samples = {}
+        for path in self.config['sourcePaths']:
+            for samplePath in glob.glob(os.path.join(path, 'Unaligned', 'Project_*', 'Sample_*')):
+                sample_name = os.path.basename(samplePath).replace('Sample_', '')
+                if sample_name in self.all_samples:
+                    raise ConfigurationException("Duplicate sample: " + sample_name)
+                self.all_samples[sample_name] = {}
+                self.all_samples[sample_name]['path'] = samplePath
+
+    def __str__(self):
+        s = ''
+        s += "Number of samples: " + str(len(self.all_samples)) + ".\n"
+        for sample in sorted(self.all_samples.keys()):
+            s += sample + "\n"
+        return s
+    
