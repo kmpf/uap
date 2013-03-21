fe19a579daee391809143fb12b9ed698e202a18e
Michael Specht <micha.specht@gmail.com>
2013-03-21 11:48:47 +0100

    arg parsing fix

--- status.py
+++ status.py
@@ -6,10 +6,11 @@ import pipeline
 import yaml
 
 run_mode = pipeline.Pipeline.run_modes.FULL
-if sys.argv[1] == '--dry-run':
-    run_mode = pipeline.Pipeline.run_modes.DRY_RUN
-elif sys.argv[1] == '--test-run':
-    run_mode = pipeline.Pipeline.run_modes.TEST_RUN
+if len(sys.argv) > 1:
+    if sys.argv[1] == '--dry-run':
+        run_mode = pipeline.Pipeline.run_modes.DRY_RUN
+    elif sys.argv[1] == '--test-run':
+        run_mode = pipeline.Pipeline.run_modes.TEST_RUN
 
 p = pipeline.Pipeline(run_mode)
 
