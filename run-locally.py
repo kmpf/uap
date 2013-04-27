c8045cd622b1b7b6339743a5d16da8c7bc5d3426
Michael Specht <micha.specht@gmail.com>
2013-04-27 21:45:15 +0200

    run-locally.py: also kill all child processes on crash exit when a certain task has been specified

--- run-locally.py
+++ run-locally.py
@@ -11,24 +11,23 @@ def main():
     p = pipeline.Pipeline()
 
     if len(sys.argv) > 1:
-        # execute the specified task
+        # execute the specified tasks
         for task_id in sys.argv[1:]:
             task = p.task_for_task_id[task_id]
             task.run()
-        exit(0)
-
-    task_list = copy.deepcopy(p.all_tasks)
-
-    try:
+    else:
+        # execute all tasks
+        task_list = copy.deepcopy(p.all_tasks)
         while p.has_unfinished_tasks(task_list):
             task = p.pick_next_ready_task(task_list)
             task_list.remove(task)
             task.run()
+
+if __name__ == '__main__':
+    try:
+        main()
     finally:
         # make sure we kill all child processes on exit
         # TODO: Will this also kill child processes which have not been
         # forked from our script? If yes, that would be a problem.
         unix_pipeline.kill_all_child_processes()
-
-if __name__ == '__main__':
-    main()
