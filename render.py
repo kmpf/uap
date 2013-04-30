db55f206b126c16ae56ab13b00c870ca833a9388
Michael Specht <micha.specht@gmail.com>
2013-04-30 20:44:13 +0200

    render.py: use a gradient to visualize step progress

--- render.py
+++ render.py
@@ -6,6 +6,51 @@ import pipeline
 import subprocess
 import yaml
 
+GRADIENTS = {
+    'burn': [
+        [0.0, '#ffffff'],
+        [0.2, '#fce94f'],
+        [0.4, '#fcaf3e'],
+        [0.7, '#a40000'],
+        [1.0, '#000000']
+    ],
+    'green': [
+        [0.0, '#ffffff'],
+        [1.0, '#4e9a06']
+    ],
+    'traffic_lights': [
+        [0.0, '#d5291a'],
+        [0.5, '#fce94f'],
+        [1.0, '#73a946']
+    ]
+}
+
+
+def mix(a, b, amount):
+    rA = float(int(a[1:3], 16)) / 255.0
+    gA = float(int(a[3:5], 16)) / 255.0
+    bA = float(int(a[5:7], 16)) / 255.0
+    rB = float(int(b[1:3], 16)) / 255.0
+    gB = float(int(b[3:5], 16)) / 255.0
+    bB = float(int(b[5:7], 16)) / 255.0
+    rC = rB * amount + rA * (1.0 - amount)
+    gC = gB * amount + gA * (1.0 - amount)
+    bC = bB * amount + bA * (1.0 - amount)
+    result = '#%02x%02x%02x' % (int(rC * 255.0), int(gC * 255.0), int(bC * 255.0))
+    return result
+
+
+def gradient(x, gradient):
+    x = max(x, 0.0)
+    x = min(x, 1.0)
+    i = 0
+    while (i < len(gradient) - 2 and gradient[i + 1][0] < x):
+        i += 1
+    colorA = gradient[i][1]
+    colorB = gradient[i + 1][1]
+    return mix(colorA, colorB, (x - gradient[i][0]) / (gradient[i + 1][0] - gradient[i][0]))
+
+
 def main():
     p = pipeline.Pipeline()
     
@@ -32,7 +77,9 @@ def main():
         if step_name != step.__module__:
             label = "%s\\n(%s)" % (step_name, step.__module__)
         f.write("    %s [label=\"%s\", style = filled, fillcolor = \"#fce94f\"];\n" % (step_name, label))
-        f.write("    %s_progress [label=\"%1.1f%%\", style = filled, height = 0.3];\n" % (step_name, float(finished_runs) * 100.0 / total_runs))
+        color = gradient(float(finished_runs) / total_runs, GRADIENTS['traffic_lights'])
+        color = mix(color, '#ffffff', 0.5)
+        f.write("    %s_progress [label=\"%1.0f%%\", style = filled, fillcolor = \"%s\" height = 0.3];\n" % (step_name, float(finished_runs) * 100.0 / total_runs, color))
         f.write("    %s -> %s_progress [arrowsize = 0];\n" % (step_name, step_name))
         f.write("    {rank=same; %s %s_progress}\n" % (step_name, step_name))
         
@@ -40,16 +87,16 @@ def main():
             connection_key = ('%s/%s' % (step_name, c)).replace('/', '__')
             f.write("    %s [label=\"%s\", shape = ellipse, fontsize = 10];\n" % (connection_key, c))
             if c[0:3] == 'in/':
-                f.write("    %s -> %s [style=dashed, color = \"#888a85\"];\n" % (connection_key, step_name))
+                f.write("    %s -> %s;\n" % (connection_key, step_name))
             else:
-                f.write("    %s -> %s [style=dashed, color = \"#888a85\"];\n" % (step_name, connection_key))
+                f.write("    %s -> %s;\n" % (step_name, connection_key))
                 
         f.write("  graph[style=dashed];\n")
         f.write("}\n")
             
     for step_name, step in p.steps.items():
         for other_step in step.dependencies:
-            f.write("    %s -> %s;\n" % (other_step.get_step_name(), step_name))
+            #f.write("    %s -> %s;\n" % (other_step.get_step_name(), step_name))
             
             for in_key in step._connections:
                 if in_key[0:3] != 'in/':
@@ -77,7 +124,7 @@ def main():
                     if out_key == real_outkey:
                         connection_key = ('%s/%s' % (step_name, in_key)).replace('/', '__')
                         other_connection_key = ('%s/%s' % (other_step.get_step_name(), out_key)).replace('/', '__')
-                        f.write("    %s -> %s [style=dashed, color = \"#888a85\"];\n" % (other_connection_key, connection_key))
+                        f.write("    %s -> %s;\n" % (other_connection_key, connection_key))
     f.write("}\n")
     
     dot.stdin.close()
