383227fdd37d602f2af35fc52150312e99c98cb1
Michael Specht <micha.specht@gmail.com>
2013-05-02 20:00:48 +0200

    mini changes in qsub template

--- qsub-template.sh
+++ qsub-template.sh
@@ -4,10 +4,10 @@
 #$ -cwd                       # Execute the job from the current working directory. This switch will activate Sun Grid Engine’s path aliasing facility, if the corresponding configuration files are present (see sge_aliases(5)).
 #$ -V                         # Specifies that all environment variables active within the qsub utility be exported to the context of the job.
 #$ -S /bin/bash               # shell to be used
-#$ -m eas                     # e-mail notification at following events: End of job, Abortion of job and Suspension of job
+#$ -m as                     # e-mail notification at following events: End of job, Abortion of job and Suspension of job
 #$ -M #{EMAIL}                # e-mail notification
 #$ -l h_rt=96:00:00           # runtime of your job
-#$ -l m_core=#{CORES}         # use only nodes where all 8 core are available
+#$ -l m_core=#{CORES}         # use only nodes where all #{CORES} cores are available
 
 # loading important environment stuff
 [ -r ~/.bashrc ]       && . ~/.bashrc
