33ed7b142df1947577b7ad45db5a9f3a8abb610a
Michael Specht <micha.specht@gmail.com>
2013-07-31 13:46:23 +0200

    streamlined grep and head -> they're super slim now and other simple I/O tools can be added easily now
    grep and head are both subclasses of IOStep now, here's how it works (take a look at grep.py and head.py):
    - 1. Call the IOStep constructor with the name of the tool as an argument and add tool-specific options.
    - 2. Implement tool_command_line() to convert these options into a meaningful string list which can be passed to the tool
    - 3. $$$

---
+++ include/steps/io_step.py
@@ -0,0 +1,59 @@
+import sys
+import abstract_step 
+import copy
+import misc
+import os
+import pipeline
+import process_pool
+
+
+class IOStep(abstract_step.AbstractStep):
+    
+    def __init__(self, pipeline, tool):
+        super(IOStep, self).__init__(pipeline)
+        
+        self._tool = tool
+        
+        self.set_cores(4)
+        self.add_connection('in/*')
+        
+        self.require_tool('cat4m')
+        self.require_tool('pigz')
+        self.require_tool(self._tool)
+        
+
+    def declare_runs(self):
+        for run_id, input_paths in self.run_ids_and_input_files_for_connection('in/*'):
+            with self.declare_run(run_id) as run:
+                for in_path in input_paths:
+                    annotation = self.annotation_for_input_file(in_path)
+                    self.add_connection('out/%s' % annotation)
+                    run.add_output_file(annotation, misc.append_suffix_to_path(os.path.basename(in_path), self._tool), [in_path])
+
+    
+    def execute(self, run_id, run):
+        # process one file at a time
+        for tag, output_file_info in run.output_files().items():
+            for output_path, input_paths in output_file_info.items():
+                with process_pool.ProcessPool(self) as pool:
+                    with pool.Pipeline(pool) as pipeline:
+                        cat4m = [self.tool('cat4m'), input_paths[0]]
+                        pigz1 = [self.tool('pigz'), '--decompress', '--processes', '1', '--stdout']
+                        tool_command = [self.tool(self._tool)]
+                        tool_command.extend(self.tool_command_line())
+                        pigz2 = [self.tool('pigz'), '--processes', '2', '--blocksize', '4096', '--stdout']
+                
+                        if output_path[-3:] == '.gz':
+                            pipeline.append(cat4m)
+                            pipeline.append(pigz1)
+                            pipeline.append(tool_command)
+                            pipeline.append(pigz2, stdout_path = output_path)
+                        else:
+                            pipeline.append(cat4m)
+                            pipeline.append(tool_command, stdout_path = output_path)
+
+    def tool_command_line(self):
+        '''
+        Raise NotImplementedError because every subclass must override this method.
+        '''
+        raise NotImplementedError()
