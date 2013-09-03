e3335ff05e66b0da76a2ac6585a7aba515026410
Christop Kaempf <christop.kaempf@ufz.de>
2013-09-03 14:35:59 +0200

    Reverse Complement for Index R2 in dual-index libraries

--- include/sources/run_folder_source.py
+++ include/sources/run_folder_source.py
@@ -2,6 +2,7 @@ import sys
 from abstract_step import *
 import csv
 import glob
+import string
 import os
 import pipeline
 import yaml
@@ -87,8 +88,11 @@ class RunFolderSource(AbstractSourceStep):
 
                     if len(index) == 2:
                         if not run.has_public_info('index-R1') and not run.has_public_info('index-R2'):
-                            run.add_public_info('index-R1', index[0])
-                            run.add_public_info('index-R2', index[1])
+                            index_r1 = index[0]
+                            complements = string.maketrans('acgtACGT', 'tgcaTGCA')
+                            index_r2 = index[1].translate(complements)[::-1]
+                            run.add_public_info('index-R1', index_r1)
+                            run.add_public_info('index-R2', index_r2)
                         else: 
                             stored_index = (run.get_public_info('index-R1') + '-' 
                                             + run.get_public_info('index-R2'))
