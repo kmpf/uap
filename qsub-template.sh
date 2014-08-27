6aa4e9a4707ace095c713f82e5292b19bca50b4f
Christop Kaempf <christop.kaempf@ufz.de>
2014-08-27 11:21:23 +0200

    CK: Removed '#$ -V' option; fixes issue with CentOS 6

--- qsub-template.sh
+++ qsub-template.sh
@@ -2,7 +2,7 @@
 
 #$ -pe smp #{CORES}           # request the "smp" parallel environment and up to #{CORES} slots
 #$ -cwd                       # Execute the job from the current working directory. This switch will activate Sun Grid Engine’s path aliasing facility, if the corresponding configuration files are present (see sge_aliases(5)).
-#$ -V                         # Specifies that all environment variables active within the qsub utility be exported to the context of the job.
+##$ -V                         # Specifies that all environment variables active within the qsub utility be exported to the context of the job.
 #$ -S /bin/bash               # shell to be used
 #$ -m as                      # e-mail notification on aborting and suspending jobs
 #$ -M #{EMAIL}                # e-mail notification address
@@ -18,15 +18,5 @@
 ## does the actual work
 ##
 source /etc/profile.d/000-modules.sh    # makes the module system available to your job
-export PATH=$PATH:~reichek/Tools/bowtie2-2.1.0/
-module load python/2.7.3-1-virtual
-module load git
-module load graphviz
-module load pigz
-module load jdk
-module load samtools
-module load boost
-module load tophat
-module load bedtools
 
 #{COMMAND}
