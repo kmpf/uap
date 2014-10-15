331567c161480e888e2d9250424fca18cfddb199
Christop Kaempf <christop.kaempf@ufz.de>
2014-10-15 13:56:01 +0200

    CK: Fixed bugs in cout_mapped_reads.py

--- include/steps/count_mapped_reads.py
+++ include/steps/count_mapped_reads.py
@@ -22,50 +22,40 @@ class CountMappedReads(AbstractStep):
 
         self.add_option('set_FLAG_bits', list, default=0)
         self.add_option('unset_FLAG_bits', list, default=0)
-        self.add_option('exclude_MAPQ_smaller_than', list, default=0)
+        self.add_option('exclude_MAPQ_smaller_than', int, default=0)
 
     def declare_runs(self):
         # set_FLAG_bits and unset_FLAG_bits must have equal length,
         # because both are set everytime samtools is called
         set_bits = self.get_option('set_FLAG_bits')
         unset_bits = self.get_option('unset_FLAG_bits')
-
+        
         if len(set_bits) != len(unset_bits):
             raise StandardError("set_FLAG_bits has %s elements but "
                                 "unset_FLAG_bits has %s elements. Both "
                                 "options need to have same length.")
-
+        
         # Get run_id and input files for 'in/alignment' connection
         sample_mapped_paths_dict = dict()
         sample_unmapped_paths_dict = dict()
         alignment_files = list()
         unmapped_files = list()
 
-        for run_id, alignments_paths in self.get_run_ids_and_input_files_for_connection('in/alignments'):
+        for run_id, alignment_paths in self.get_run_ids_and_input_files_for_connection('in/alignments'):
             # Get the according input files for 'in/unmapped'
-            unmapped_paths = get_input_files_for_run_id_and_connection(
+            unmapped_paths = self.get_input_files_for_run_id_and_connection(
                 run_id, 'in/unmapped')
             # Only one BAM/SAM file expected as input file
-            if len(alignments_paths) != 1:
+            if len(alignment_paths) != 1:
                 raise StandardError("Expected exactly one file with mapped reads.")
                 
             if len(unmapped_paths) != 1:
                 raise StandardError("Expected exactly one file with unmapped reads.")
-            sample_mapped_paths_dict[run_id] = alignments_paths[0]
+            sample_mapped_paths_dict[run_id] = alignment_paths[0]
             sample_unmapped_paths_dict[run_id] = unmapped_paths[0]
-            alignments_files.extend(alignment_paths)
+            alignment_files.extend(alignment_paths)
             unmapped_files.extend(unmapped_paths)
-            
-
-#            for name, files in 
-#            {'mapped': alignments_paths, 'unmapped': unmapped_paths}.iteritems():
-#                # Create a list of files to store the temporary results in
- ##               temp_count_files = []
- #               for i in range(len(set_bits)):
- #                   count_file = "%s-f_%s_-F_%s.txt" % 
- #                   (basename, set_bits[i], unset_bits[i])
- #                   temp_count_files.append(count_file)
-                    
+                                
         # Name the run as the step
         run_id = self.get_step_name()
 
@@ -76,7 +66,7 @@ class CountMappedReads(AbstractStep):
                                  sample_unmapped_paths_dict)
             run.add_output_file('mapped-counts', 
                                 '%s_mapped_counts.txt' % run_id,
-                                alignments_files)
+                                alignment_files)
             run.add_output_file('unmapped-counts',
                                 '%s_unmapped_counts.txt' % run_id,
                                 unmapped_files)
@@ -106,12 +96,12 @@ class CountMappedReads(AbstractStep):
         unset_bits = self.get_option('unset_FLAG_bits')
 
         mapped_counts = open(out_mapped_path, 'w')
-        header = ["FLAGS_SET", "FLAGS_UNSET", "COUNTS"]
+        header = ["SAMPLE","FLAGS_SET", "FLAGS_UNSET", "COUNTS"]
         mapped_counts.write( ",".join(header) + "\n")
         
         for sample, input_path in sample_mapped_paths_dict.iteritems():
             for i in range(len(self.get_option('set_FLAG_bits'))):
-                out_file = temp_os.path.join(
+                out_file = os.path.join(
                     temp_mapped_dir, "%s-f_%s_-F_%s.txt" % 
                     (sample, set_bits[i], unset_bits[i]))
                 
@@ -121,8 +111,8 @@ class CountMappedReads(AbstractStep):
                         samtools = [self.get_tool('samtools'), 'view', '-c',
                                     '-f', unset_bits[i],
                                     '-F', set_bits[i],
-                                    '-q', self.get_option(
-                                        'exclude_MAPQ_smaller_than'),
+                                    '-q', str(self.get_option(
+                                        'exclude_MAPQ_smaller_than')),
                                     '-', '-o', '-']
                                         
                         pipeline.append(cat4m)
@@ -133,8 +123,9 @@ class CountMappedReads(AbstractStep):
                 f.close()
 
                 mapped_counts = open(out_mapped_path, 'a')
-                mapped_counts.write("%s,%s,%s" % 
-                                    (set_bits[i], unset_bits[i], sample))
+                mapped_counts.write("%s,%s,%s,%s\n" % 
+                                    (sample, set_bits[i], 
+                                     unset_bits[i], counts))
 
         unmapped_counts = open(out_unmapped_path, 'w')
         header = ["", ""]
@@ -152,8 +143,8 @@ class CountMappedReads(AbstractStep):
                         samtools = [self.get_tool('samtools'), 'view', '-c',
                                     '-f', unset_bits[i],
                                     '-F', set_bits[i],
-                                    '-q', self.get_option(
-                                        'exclude_MAPQ_smaller_than'),
+                                    '-q', str(self.get_option(
+                                        'exclude_MAPQ_smaller_than')),
                                     '-', '-o', '-']
                                         
                         pipeline.append(cat4m)
@@ -164,5 +155,6 @@ class CountMappedReads(AbstractStep):
                 f.close()
 
                 unmapped_counts = open(out_unmapped_path, 'a')
-                unmapped_counts.write("%s,%s,%s" % 
-                                    (set_bits[i], unset_bits[i], sample))
+                unmapped_counts.write("%s,%s,%s,%s\n" % 
+                                      (sample, set_bits[i], 
+                                       unset_bits[i], counts))
