d51662d1350199c925b073b5861349e6783ea93e
Christoph Kaempf <christoph.kaempf@ufz.de>
2015-04-07 23:09:05 +0200

    CK: Removed seq-pipeline and first attempt to add fifo creation

--- include/exec_group.py
+++ include/exec_group.py
@@ -13,8 +13,9 @@ class ExecGroup(object):
     def __init__(self, run):
         self._run = run
         self._pipes_and_commands = list()
+        self._fifos = dict()
 
-    def __enter__(self):
+    def __enter__(self):        
         return self
 
     def __exit__(self, type, value, traceback):
@@ -25,9 +26,11 @@ class ExecGroup(object):
         self._pipes_and_commands.append(pipeline)
         return pipeline
 
-    def new_command(self, command, stderr_path=None, stdout_path=None):
-        command = command_info.CommandInfo(self, command, stderr_path,
-                                            stdout_path)
+    def new_command(self, command, stdout_path=None, stderr_path=None):
+        command = command_info.CommandInfo(self, command, 
+                                           stdout_path = stdout_path, 
+                                           stderr_path = stderr_path
+                                       )
         self._pipes_and_commands.append(command)
         return command
 
@@ -36,3 +39,12 @@ class ExecGroup(object):
 
     def get_run(self):
         return self._run
+
+    def new_temporary_fifo(self, prefix, designation = None):
+        placeholder = "<temp-fifo-%s-%s>" % (prefix, designation)
+        self._fifos[placeholder] = {'prefix' : prefix,
+                                    'designation': designation}
+        return placeholder
+
+    def get_temporary_fifo(self, placeholder):
+        return self._fifos[placeholder]
