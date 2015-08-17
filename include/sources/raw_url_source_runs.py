3373f29cad18eb9ba138b41cce628fb194d71317
Christoph Kaempf <kaempf@bioinf.uni-leipzig.de>
2015-08-17 16:13:29 +0200

    [rl7165] Replace declare_runs()+execute() by runs()

--- include/sources/raw_url_source_runs.py
+++ include/sources/raw_url_source_runs.py
@@ -15,12 +15,13 @@ class RawUrlSource(AbstractStep):
 
         self.require_tool('compare_secure_hashes')
         self.require_tool('curl')
+        self.require_tool('mv')
 
         self.add_option('filename', str, optional = True,
                         description = "local file name of downloaded file")
         self.add_option('hashing-algorithm', str, optional=False,
                         choices = ['md5', 'sha1', 'sha224', 'sha256',
-                                   'sha384', 'sha512']
+                                   'sha384', 'sha512'],
                         description = "hashing algorithm to use")
         self.add_option('secure-hash', str, optional = False,
                         description = "expected secure hash of downloaded file")
@@ -35,18 +36,22 @@ class RawUrlSource(AbstractStep):
             filename = self.get_option('filename')
 
         with self.declare_run('download') as run:
-            run.add_output_file('raw', filename, [])
-            with run.new_exec_group() as exec_group:
+            out_file = run.add_output_file('raw', filename, [])
+            temp_filename = run.add_temporary_file(filename)
+            with run.new_exec_group() as curl_exec_group:
                 # 1. download file
                 curl = [self.get_tool('curl'), self.get_option('url')]
-                exec_group.add_command(curl)
-                curl_pipe.append(curl, stdout_path = filename)
+                curl_exec_group.add_command(curl, stdout_path = temp_filename)
+            with run.new_exec_group() as check_exec_group:
                 # 2. Compare secure hashes
                 compare_secure_hashes = [self.get_tool('compare_secure_hashes'),
-                                     '--algorithm',
-                                     self.get_option('hashing-algorithm'),
-                                     '--secure-hash',
-                                     self.get_option('secure-hash'),
-                                     self.get_option('filename')]
-                exec_group.add_command(compare_secure_hashes)
-
+                                         '--algorithm',
+                                         self.get_option('hashing-algorithm'),
+                                         '--secure-hash',
+                                         self.get_option('secure-hash'),
+                                         temp_filename
+                                     ]
+                check_exec_group.add_command(compare_secure_hashes)
+            with run.new_exec_group() as mv_exec_group:
+                mv = [self.get_tool('mv'), temp_filename, out_file]
+                mv_exec_group.add_command(mv)
