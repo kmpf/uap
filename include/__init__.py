aef589c54e58f261864cbca9dae859fb830296fa
Christoph Kaempf <kaempf@bioinf.uni-leipzig.de>
2015-08-19 11:42:16 +0200

    [an2857] Make uap python3 compatible

--- include/__init__.py
+++ include/__init__.py
@@ -1,13 +1,13 @@
-import abstract_step
-import command
-import exec_group
-import fscache
-import misc
-import pipeline
-import pipeline_info
-import process_pool
-import run
-import task
+from . import abstract_step
+from . import command
+from . import exec_group
+from . import fscache
+from . import misc
+from . import pipeline
+from . import pipeline_info
+from . import process_pool
+from . import run
+from . import task
 
 __all__ = ['abstract_step', 'command', 'exec_group', 'fscache', 'misc',
            'pipeline', 'pipeline_info', 'process_pool', 'run', 'task']
