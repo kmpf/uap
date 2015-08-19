d79c1f0e15bdc64e1f106f590ae9d36c37545ffb
Christoph Kaempf <kaempf@bioinf.uni-leipzig.de>
2015-08-19 11:28:00 +0200

    [an2857] Make uap python3 compatible

--- include/subcommands/__init__.py
+++ include/subcommands/__init__.py
@@ -1,8 +1,8 @@
-import fix_problems
-import render
-import run_locally
-import status
-import submit_to_cluster
-import volatilize
+from . import fix_problems
+from . import render
+from . import run_locally
+from . import status
+from . import submit_to_cluster
+from . import volatilize
 __all__ = ['fix_problems', 'render', 'run_locally',
            'status', 'submit_to_cluster', 'volatilize']
