585824ce92b2de655ad1a063420eb0becb9b3b59
Christoph Kaempf <kaempf@bioinf.uni-leipzig.de>
2015-09-04 14:02:16 +0200

    Revert "[rl7165] Replace declare_runs()+execute() by runs()"
    This reverts commit a2cdfa856bcf2e714c60f5ec26476519c9363ce0.

--- include/abstract_step.py
+++ include/abstract_step.py
@@ -273,7 +273,7 @@ class AbstractStep(object):
         
         self.runs(run_ids_connections_files)
         
-    def runs(self, run_ids_connections_files):
+    def runs(self):
         """Abstract method this must be implemented by actual step.
 
         Raise NotImplementedError if subclass does not override this
