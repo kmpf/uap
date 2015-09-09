f4d09dbbc2b59c835364c543a177c4131fdf0aee
Christoph Kaempf <christoph.kaempf@ufz.de>
2015-09-09 16:40:18 +0200

    CK: Added uncompress to raw_url_source_runs.py

--- include/sources/raw_url_source_runs.py
+++ include/sources/raw_url_source_runs.py
@@ -17,6 +17,7 @@ class RawUrlSource(AbstractSourceStep):
         self.require_tool('curl')
         self.require_tool('mkdir')
         self.require_tool('mv')
+        self.require_tool('pigz')
 
         self.add_option('filename', str, optional = True,
                         description = "local file name of downloaded file")
@@ -28,7 +29,10 @@ class RawUrlSource(AbstractSourceStep):
                         description = "directory to move downloaded file to")
         self.add_option('secure-hash', str, optional = False,
                         description = "expected secure hash of downloaded file")
-        self.add_option('url', str, optional=False,
+        self.add_option('uncompress', bool, optional = True, default = False,
+                        description = 'Shall the file be uncompressed after '
+                        'downloading')
+        self.add_option('url', str, optional = False,
                         description = "file URL")
         
     def runs(self, run_ids_connections_files):
@@ -54,9 +58,8 @@ class RawUrlSource(AbstractSourceStep):
                 with run.new_exec_group() as mkdir_exec_group:
                     mkdir = [self.get_tool('mkdir'), '-p', path]
                     mkdir_exec_group.add_command(mkdir)
-            out_file = run.add_output_file(
-                'raw', final_abspath, [] )
-            temp_filename = run.add_temporary_file(filename)
+            out_file = run.add_output_file('raw', final_abspath, [] )
+            temp_filename = run.add_temporary_file(suffix = filename)
             with run.new_exec_group() as curl_exec_group:
                 # 1. download file
                 curl = [self.get_tool('curl'), self.get_option('url')]
@@ -72,5 +75,11 @@ class RawUrlSource(AbstractSourceStep):
                                      ]
                 check_exec_group.add_command(compare_secure_hashes)
             with run.new_exec_group() as mv_exec_group:
+                if self.get_option("uncompress"):
+                    pigz = [self.get_tool('pigz'), '--decompress',
+                            '--processes', '1', temp_filename]
+                    temp_filename = os.path.splitext(temp_filename)[0]
+                    mv_exec_group.add_command(pigz)
+
                 mv = [self.get_tool('mv'), temp_filename, out_file]
                 mv_exec_group.add_command(mv)
