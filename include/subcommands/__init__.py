d27e425692458a35fdf1c2490d701688a4b2f2de
Christoph Kaempf <kaempf@bioinf.uni-leipzig.de>
2015-11-10 15:30:51 +0100

    [sl6820] Add option to display commands executed by the pipeline

--- include/subcommands/__init__.py
+++ include/subcommands/__init__.py
@@ -4,6 +4,7 @@ import report
 import run_locally
 import status
 import submit_to_cluster
+import task_info
 import volatilize
 __all__ = ['fix_problems', 'render', 'report', 'run_locally', 'status',
-           'submit_to_cluster', 'volatilize']
+           'submit_to_cluster', 'task_info', 'volatilize']
