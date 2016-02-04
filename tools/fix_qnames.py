da7be52117c6ba3b3b471a41794d7ee509e2e332
Christoph Kaempf <kaempf@bioinf.uni-leipzig.de>
2016-02-04 14:39:10 +0100

    [lf9495] Create separate fix_qnames step

--- tools/fix_qnames.py
+++ tools/fix_qnames.py
@@ -3,20 +3,63 @@ import os
 seq_pipeline_path = os.path.dirname(os.path.realpath(__file__))
 activate_this_file = '%s/../python_env/bin/activate_this.py' % seq_pipeline_path
 execfile(activate_this_file, dict(__file__=activate_this_file))
+import argparse
 import sys
 
 def main():
-    while True:
-        lines = []
-        for _ in range(4):
-            lines.append(sys.stdin.readline())
-        if (len(lines[0]) == 0):                                                                                                         
-            break
-        lines[0] = lines[0].split(' ')[0]
-        if lines[0][-1] != "\n":
-            lines[0] += "\n"
-        for _ in range(4):
-            sys.stdout.write(lines[_])
+
+    # Definition of the argument parser
+    
+    parser = argparse.ArgumentParser(
+        description="This script is able to repair non-standard QNAMEs in FASTQ"
+        " or SAM files.",
+        formatter_class=argparse.RawTextHelpFormatter
+        )
+    
+    parser.add_argument(
+        "input_file", nargs='?', type=argparse.FileType('r'), default=sys.stdin,
+        help="Name of input file where QNAMES need to be "
+        "fixed. Reads by default from STDIN."
+    )
+
+    parser.add_argument(
+        "output_file", nargs='?', type=argparse.FileType('w'), default=sys.stdout,
+        help="Name of output file to which corrected data is written to. "
+        "Reads by default from STDOUT.",
+    )
+    
+    parser.add_argument(
+        "-f", "--filetype", type=str, choices=['FASTQ', 'SAM'], default='FASTQ',
+        help="File type of the provided input data. Can be 'FASTQ' or 'SAM'."
+        "Default is 'FASTQ'."
+    )
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    if args.filetype == 'FASTQ':
+        while True:
+            lines = []
+            for _ in range(4):
+                lines.append(args.input_file.readline())
+            if (len(lines[0]) == 0):                                                                                                         
+                break
+            lines[0] = lines[0].split(' ')[0]
+            if lines[0][-1] != "\n":
+                lines[0] += "\n"
+            for _ in range(4):
+                args.output_file.write(lines[_])
+
+    elif args.filetype == 'SAM':
+        for l in args.input_file:
+            line = l.split('\t')
+            line[0] = line[0].split(' ')[0]
+            args.output_file.write("\t".join(line))
+
+
+    args.input_file.close()
+    args.output_file.close()
+
 
 if __name__ == '__main__':
     main()
