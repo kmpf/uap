45d0edd23346aa8d38878dff5380a997b557b79a
Jana Hertel <jana.hertel@ufz.de>
2016-06-02 15:01:08 +0200

    segemehl cores reset to 10

--- include/steps/segemehl.py
+++ include/steps/segemehl.py
@@ -36,7 +36,7 @@ class Segemehl(AbstractStep):
     def __init__(self, pipeline):
         super(Segemehl, self).__init__(pipeline)
 
-        self.set_cores(12) # set # of cores for cluster, it is ignored if run locally
+        self.set_cores(10) # set # of cores for cluster, it is ignored if run locally
 
         # connections - indentifier for in/output
         #             - expects list, maybe empty or 'none', e.g. if only first_read info available
