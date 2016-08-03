090358772758c8991b84dea6f6066c7b2f7acc3e
Jana Hertel <jana.hertel@ufz.de>
2016-08-03 15:08:07 +0200

    spaces removed in options of samtools

--- include/steps/subsetMappedReads.py
+++ include/steps/subsetMappedReads.py
@@ -86,13 +86,13 @@ class subsetMappedReads(AbstractStep):
                         # extract only reads that were aligned and include only pairs
                         if self.get_option('paired_end'):
                             samtools_view = [
-                                self.get_tool('samtools'), 'view', '-F 0x04 -f 0x03', '-h',
+                                self.get_tool('samtools'), 'view', '-F', '0x04', '-f', '0x03', '-h',
 #                                '-t', self.get_option('genome-faidx'),
                                 '-'
                             ]
                         else:
                             samtools_view = [
-                                self.get_tool('samtools'), 'view', '-F 0x04', '-h',
+                                self.get_tool('samtools'), 'view', '-F', '0x04', '-h',
 #                                self.get_tool('samtools'), 'view', '-c',
 #                                '-t', self.get_option('genome-faidx'),
                                 '-'
