bf5a26d804d5d54f535f5a354069102fcbc1bf49
Alexander Scholz <alexander.scholz@izi.fraunhofer.de>
2017-01-19 11:54:34 +0100

    added cluster directory

---
+++ cluster/submit-scripts/qsub-template.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+##################
+# Submit Options #
+##################
+
+#$ #{SUBMIT_OPTIONS}          # User defined submit options
+
+############
+# Commands #
+############
+
+#{PRE_JOB_COMMAND}
+
+#{COMMAND}
+
+#{POST_JOB_COMMAND}
