44497d887c2dccd1c3ba41f8c74d9ea6f27cb712
Alexander Scholz <alexander.scholz@izi.fraunhofer.de>
2017-04-07 12:13:41 +0200

    added further rseqc-subcommands, outcommentet

--- include/steps/rseqc.py
+++ include/steps/rseqc.py
@@ -85,3 +85,93 @@ class RSeQC(AbstractStep):
                             basename + '.read_distribution.txt', alignments
                         )
                     )
+
+                #with run.new_exec_group() as exec_group:
+                #    gene_body_coverage = [
+                #        self.get_tool('gene_body_coverage.py'),
+                #        '-i', alignments[0],
+                #        '-r', self.get_option('reference')
+                #        # todo: -o?
+                #    ]
+                    #exec_group.add_command(
+                    #    gene_body_coverage,
+                    #    stdout_path=run.add_output_file(
+                    #        'gene_body_coverage',
+                    #        basename + '.gene_body_coverage.txt', alignments
+                    #    )
+                    #)
+
+               # with run.new_exec_group() as exec_group:
+               #     inner_distance = [
+               #         self.get_tool('inner_distance.py'),
+               #         '-i', alignments[0],
+               #         '-r', self.get_option('reference')
+               #         # todo: -o?
+               #     ]
+                    #exec_group.add_command(
+                    #    inner_distance,
+                    #    stdout_path=run.add_output_file(
+                    #        'inner_distance',
+                    #        basename + '.inner_distance.txt', alignments
+                    #    )
+                    #)
+
+                #with run.new_exec_group() as exec_group:
+                #    junction_annotation = [
+                #        self.get_tool('junction_annotation.py'),
+                #        '-i', alignments[0],
+                #        '-r', self.get_option('reference')
+                #        # todo: -o?
+                #    ]
+                    #exec_group.add_command(
+                    #    junction_annotation,
+                    #    stdout_path=run.add_output_file(
+                    #        'junction_annotation',
+                    #        basename + '.junction_annotation.txt', alignments
+                    #    )
+                    #)
+
+                #with run.new_exec_group() as exec_group:
+                #    junction_saturation = [
+                #        self.get_tool('junction_saturation.py'),
+                #        '-i', alignments[0],
+                #        '-r', self.get_option('reference')
+                #        # todo: -o?
+                #    ]
+                    #exec_group.add_command(
+                    #    junction_saturation,
+                    #    stdout_path=run.add_output_file(
+                    #        'junction_saturation',
+                    #        basename + '.junction_saturation.txt', alignments
+                    #    )
+                    #)
+
+                #with run.new_exec_group() as exec_group:
+                #    read_duplication = [
+                #        self.get_tool('read_duplication.py'),
+                #        '-i', alignments[0],
+                #        '-r', self.get_option('reference')
+                #        # todo: -o?
+                #    ]
+                    #exec_group.add_command(
+                    #    read_duplication,
+                    #    stdout_path=run.add_output_file(
+                    #        'read_duplication',
+                    #        basename + '.read_duplication.txt', alignments
+                    #    )
+                    #)
+
+                #with run.new_exec_group() as exec_group:
+                #    read_gc = [
+                #        self.get_tool('read_gc.py'),
+                #        '-i', alignments[0],
+                #        '-r', self.get_option('reference')
+                #        # todo: -o?
+                #    ]
+                    #exec_group.add_command(
+                    #    read_gc,
+                    #    stdout_path=run.add_output_file(
+                    #        'read_gc',
+                    #        basename + '.read_gc.txt', alignments
+                    #    )
+                    #)
