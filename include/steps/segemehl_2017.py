567fabe0a2c1541003f4503f4c980649f7f08d03
Jana Hertel <jana.hertel@ufz.de>
2017-05-08 15:05:54 +0200

    getting temp directory placeholder

--- include/steps/segemehl_2017.py
+++ include/steps/segemehl_2017.py
@@ -304,9 +304,9 @@ class Segemehl2017(AbstractStep):
 
                         if self.is_option_set_in_config('splits'):
                             # here I can add the directory for the splits files
-#                            outdir = run.get_temp_output_directory()
-#                            prefix = '%s/%s_splits' % (outputdir, run_id)
-                            prefix = '%s_splits' %  run_id
+                            outdir = run.get_output_directory_du_jour_placeholder()
+                            prefix = '%s/%s_splits' % (outdir, run_id)
+#                            prefix = '%s_splits' %  run_id
                             segemehl.extend(['-S', prefix])
                             sngl_path = run.add_output_file('splits_sngl', '%s.sngl.bed' % prefix, input_paths)
                             run.add_output_file('splits_mult', '%s.mult.bed' % prefix, input_paths)
