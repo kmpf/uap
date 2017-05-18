dd9c48b8d1c1b09aed640eef4a1cb426036bbd43
Alexander Scholz <alexander.scholz@izi.fraunhofer.de>
2017-05-18 20:47:15 +0200

    intermediate status

--- include/subcommands/runtime_info.py
+++ include/subcommands/runtime_info.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
 
-import sys
 import logging
 import glob
 import os
-import string
 import yaml
 
 import pipeline
@@ -37,7 +35,6 @@ def main(args):
         task_list = p.all_tasks_topologically_sorted
 
     for task in task_list:
-        print(task)
         outdir = task.get_run().get_output_directory()
         anno_files = glob.glob(os.path.join(
             outdir, ".%s*.annotation.yaml" % task.get_run().get_run_id()
@@ -46,8 +43,57 @@ def main(args):
         yaml_files = {os.path.realpath(f) for f in anno_files \
                       if os.path.islink(f)}
         for y in yaml_files:
-            print(y)
+            annotation_data = Annotation_Data(y)
+            #print(annotation_data.total_start_time, annotation_data.total_end_time)
+            #print(annotation_data)
+            for property, value in vars(annotation_data).iteritems():
+                print property, ": ", value
+            pass
 
 
 if __name__ == '__main__':
     main(args)
+
+class Annotation_Data:
+
+    def __init__(self, file_path):
+        file = open(file_path, 'r')
+        self.__yaml_data = yaml.load(file)
+        self.__prepare_data()
+
+    def __prepare_data(self):
+
+        self.main_pid = self.__yaml_data['pid']
+        self.run_id = self.__yaml_data['run']['run_id']
+        self.step_id = self.__yaml_data['step']['name']
+
+        self.total_start_time = self.__yaml_data['start_time']
+        self.total_end_time = self.__yaml_data['end_time']
+        self.total_duration = str(self.total_end_time - self.total_start_time)
+
+        self.sum = self.__yaml_data['pipeline_log']['process_watcher']['max']['sum']
+        self.requested_cores = self.__yaml_data['step']['cores']
+
+        self.processes = dict()
+        for i, process_data in enumerate(self.__yaml_data['pipeline_log']['processes']):
+            tmp_data = dict()
+
+            tmp_data['start_time'] = process_data['start_time']
+            tmp_data['end_time'] = process_data['end_time']
+            tmp_data['duration'] = str(process_data['end_time'] - process_data['start_time'])
+            tmp_data['process_pid'] = process_data['pid']
+
+            tmp_data['main_tool'] = 0
+            process_name = str(process_data['start_time']) + ' ' + process_data['name']
+            if i == 0:
+                tmp_data['main_tool'] = 1
+                process_name = process_data['name']
+
+            # search in process watcher for information
+            if tmp_data['process_pid'] in self.__yaml_data['pipeline_log']['process_watcher']['max']:
+                tmp_data.update(self.__yaml_data['pipeline_log']['process_watcher']['max'][tmp_data['process_pid']])
+
+            self.processes[process_name] = dict()
+            self.processes[process_name] = tmp_data
+
+        #print(self.processes)
