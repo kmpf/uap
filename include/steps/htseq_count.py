42a84b513167df8a4fc76de81c440b85edf2d898
Jana Hertel <jana.hertel@ufz.de>
2017-07-25 11:22:19 +0200

    in/features inserted back again

--- include/steps/htseq_count.py
+++ include/steps/htseq_count.py
@@ -20,17 +20,17 @@ class HtSeqCount(AbstractStep):
         
         self.set_cores(2)
         
-        self.add_connection('in/alignments')
+#        self.add_connection('in/alignments')
         # the BAM files
-#        self.add_connection(
-#            'in/alignments',
-#            constraints = {'min_files_per_run': 1, 'max_files_per_run': 1}
-#        )
+        self.add_connection(
+            'in/alignments',
+            constraints = {'min_files_per_run': 1, 'max_files_per_run': 1}
+        )
 
         # the feature file provided by another step (e.g. cuffmerge)
-#        self.add_connection('in/features',
-#                            constraints = {'total_files': 1}
-#        )
+        self.add_connection('in/features',
+                            constraints = {'total_files': 1}
+        )
 
         # the counts per alignment
         self.add_connection('out/counts')
@@ -87,8 +87,7 @@ class HtSeqCount(AbstractStep):
                 input_paths = alignments
                 features_path = str
                 try:
-#                    features_path = run_ids_connections_files['magic']['in/features'][0]
-                    features_path = run_ids_connections_files['magic']['in/alignments'][0]
+                    features_path = run_ids_connections_files['magic']['in/features'][0]
                     input_paths.extend(features_path)
                 except KeyError:
                     if self.is_option_set_in_config('feature-file'):
