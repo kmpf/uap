491f1cb77f9756f14379c2cece1c7e9c7e8015bf
Schimmelpfennig <c.schimmelpfennig@ribnode011.izi.fraunhofer.de>
2018-03-07 15:56:18 +0100

    started implementation

--- include/steps/fusioncatcher.py
+++ include/steps/fusioncatcher.py
@@ -10,7 +10,7 @@ class ChimPipe(AbstractStep):
     '''
     TODO
 
-    ChimPipe is a tool to discover gene fusions in human paired-end RNA-Seq data.
+    FusionCatcher is a tool to discover gene fusions in human paired-end RNA-Seq data.
 
     Paper: https://genomebiology.biomedcentral.com/articles/10.1186/gb-2013-14-2-r12
 
@@ -33,49 +33,19 @@ class ChimPipe(AbstractStep):
         self.add_connection('out/tar_archive')
 
 # adding required tools
-        self.require_tool('chimpipe')
+        self.require_tool('fusioncatcher')
         self.require_tool('mkdir')
         self.require_tool('tar')
         self.require_tool('rm')
 
 #adding options
-        self.add_option('genome_index', str, optional=False, 
-                        description="Reference genome index in GEM-format"
-			)
-
-        self.add_option('annotation', str, optional=False,
-                        description="Reference gene annotation in gtf-format"
-                        )
-
-        self.add_option('transcriptome_index', str, optional=False,
-                        description="Annotated transcriptome index in GEM-format"
-                        )
-
-        self.add_option('transcriptome_keys', str, optional=False,
-                        description="Transcriptome to genome conversion keys"
-                        )
-        
-        self.add_option('sample_ID', str, optional=False,
-                        description="Identifier to be used in output file names"
-                        )
+        self.add_option('index', str, optional=False, 
+                        description="Path to index folder")
 
         self.add_option('cores', str, default='6')
- 
-        self.add_option('consensus_seq', str, optional = True, 
-			description="""Sequence pair of consensus splice site bases.TODO  """
-			)
-        
-	self.add_option('library_type', str, optional = True, 
-			description="TODO"
-			)
-	self.add_option('similarity', str, optional = True, 
-			description="TODO"
-			)
-
 
     def runs(self, run_ids_connections_files):
         self.set_cores(self.get_option('cores'))
-
 
         for run_id in run_ids_connections_files.keys():
             with self.declare_run(run_id) as run:
@@ -94,91 +64,56 @@ class ChimPipe(AbstractStep):
                 else:
                     input_paths.append(sr_input)
 
-
-		#create folder structure
+		# create folder structure
 
                 my_temp_dir = run.get_output_directory_du_jour_placeholder()
 
                 my_input  = os.path.join(my_temp_dir, 'input')
                 my_output = os.path.join(my_temp_dir, 'output')
-		my_cp_temp = os.path.join(my_temp_dir, 'run_temp')
 
-
-		#create logfiles                
+		# create logfiles                
                 log_stderr = run.add_output_file(
                     'log_stderr',
-                    '%s-chimpipe-log_stderr.txt' % run_id,
+                    '%s-fusioncatcher-log_stderr.txt' % run_id,
                     input_paths)
 
                 log_stdout = run.add_output_file(
                     'log_stdout',
-                    '%s-chimpipe-log_stdout.txt' % run_id,
+                    '%s-fusioncatcher-log_stdout.txt' % run_id,
                     input_paths)
-
-              
-
                 
                 # init 
                 with run.new_exec_group() as exec_group:
                     with exec_group.add_pipeline() as pseudo_init:
 			#create folders
-                        make_dirs = [ self.get_tool('mkdir'), '-p',
+                        make_dirs = [self.get_tool('mkdir'), '-p',
                                       my_input,
-                                      my_output,
-				      my_cp_temp
-                                      ]
+                                      my_output]
                         pseudo_init.add_command(make_dirs)
 			
-
-               #Assemble chimpipe command
+               # Assemble chimpipe command
                 with run.new_exec_group() as exec_group:
-                    chimpipe = [
-                        self.get_tool('chimpipe'),
-                        '--fastq_1', fr_input,
-                        '--fastq_2', sr_input,
-                        '-g', self.get_option('genome_index'), 
-                        '-a', self.get_option('annotation'),
-			'-t', self.get_option('transcriptome_index'),
-			'-k', self.get_option('transcriptome_keys'),
-			'--sample-id', self.get_option('sample_ID'),
+                    fusioncatcher = [
+                        self.get_tool('fusioncatcher'),
+                        '-d',self.get_option('index') ,
+                        '-i', my_input,
+                        '-o', my_output, 
 			'--threads', self.get_option('cores'),
-			'--tmp-dir', my_cp_temp,
-			'--output-dir', my_output,
-			'--no-cleanup',
+			'--keep-unmapped-read',
+			'--skip-filter-adapter',
+			'--extract-buffer-size=1000000000'
                     ]
 	
-
-	      	    if self.is_option_set_in_config('consensus_seq'):		       
-	      	        if self.get_option('consensus_seq'):
-	      	    	    chimpipe.extend([
-	      	    	    '-C', self.get_option('consensus_seq'),
-	      	    	    '-c', self.get_option('consensus_seq')])
-	      
-	      	    if self.is_option_set_in_config('library_type'):
-	      	        if self.get_option('library_type'):
-	      	    	    chimpipe.extend([
-	      	    	    '-l', self.get_option('library_type')])
-	      
-	      	    if self.is_option_set_in_config('similarity'):
-	      	        if self.get_option('similarity'):
-	      	    	    chimpipe.extend([
-	      	    	    '--similarity-gene-pairs', self.get_option('similarity')])
-	
-
-                    exec_group.add_command(chimpipe,  
+                    exec_group.add_command(fusioncatcher,  
                                        stderr_path=log_stderr, 
                                        stdout_path=log_stdout)
                         
-                        
-                        
-
  		with run.new_exec_group() as exec_group:
 		
 		#pack outfolder into tar/zip
 			out_archive = run.add_output_file(
 					'tar_archive',
-					'%s-chimpipe-out.tar.gz'% run_id, input_paths)
-
+					'%s-fusioncatcher-out.tar.gz'% run_id, input_paths)
 
 			tar_output = [self.get_tool('tar'),
 				'-czf', out_archive, 
@@ -190,6 +125,6 @@ class ChimPipe(AbstractStep):
                 #remove temp dir
 
                         rm_temp = [self.get_tool('rm'),
-                                '-r', my_input, my_output, my_cp_temp]
+                                '-r', my_input, my_output]
 
                         exec_group.add_command(rm_temp)
