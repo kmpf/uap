be299c66f52000f0d1e6c0a16940f85b5a167e81
tiennes <sven-holger.puppel@izi.fraunhofer.de>
2018-05-13 15:52:19 +0200

    added gffread_extract_transcripts

--- include/steps/gffread_extract_transcripts.py
+++ include/steps/gffread_extract_transcripts.py
@@ -18,6 +18,7 @@ class GffreadExtractTranscripts(AbstractStep):
         self.set_cores(1)
 
         self.add_connection('in/fasta')
+        self.add_connection('in/anno')
 
         self.add_connection('out/fasta')
         self.add_connection('out/log_stderr')
@@ -25,38 +26,52 @@ class GffreadExtractTranscripts(AbstractStep):
 
         self.require_tool('gffread')
 
-        self.add_option('gtf', str, optional=False, default=None,
+        self.add_option('gtf', str, optional=True, default=None,
                         description="path to gtf file")
 
         self.add_option('output-fasta-name', str, optional=False, default=None,
                         description="name of the outputfile trancriptom myfasta.fa")
 
     def runs(self, run_ids_connections_files):
-        for run_id in run_ids_connections_files.keys():
-            with self.declare_run(run_id) as run:
-                cmd = [self.get_tool('gffread'), '-w']
+        run_id = self.get_option('output-fasta-name')
+        # dependenency files (r for required)
+        rfiles = []
+        with self.declare_run(run_id) as run:
+            cmd = [self.get_tool('gffread'), '-w']
+            
+            path = run.get_output_directory_du_jour_placeholder()
+            cmd.append(path + '/' + self.get_option('output-fasta-name'))
 
-                path = run.get_output_directory_du_jour_placeholder()
-                cmd.append(path + '/' + self.get_option('output-fasta-name'))
+            for __ , connection  in run_ids_connections_files.items():
+                if 'in/fasta' in connection:
+                    cmd.append('-g')
+                    cmd.append(connection['in/fasta'][0])
+                    rfiles.append(connection['in/fasta'][0])
+                    continue
+                               
 
-
-                fasta_file = run_ids_connections_files[run_id]['in/fasta']
-                cmd.append('-g')
-                cmd.append(fasta_file[0])
-                
+            if self.is_option_set_in_config('gtf'):
                 cmd.append(self.get_option('gtf'))
+            else:
+                for __ , connection  in run_ids_connections_files.items():
+                    if 'in/anno' in connection:
+                        cmd.append(connection['in/anno'][0])
+                        rfiles.append(connection['in/anno'][0])
+                        continue
 
-                stderr_file = "%s-gffread_extract_transcripts-log_stderr.txt" % (run_id)
-                log_stderr = run.add_output_file("log_stderr",
-                                                 stderr_file, fasta_file)
-                stdout_file = "%s-gffread_extract_transcripts-log_stdout.txt" % (run_id)
-                log_stdout = run.add_output_file("log_stdout",
-                                                 stdout_file, fasta_file)
+                    
+            
+            stderr_file = "%s-gffread_extract_transcripts-log_stderr.txt" % (run_id)
+            log_stderr = run.add_output_file("log_stderr",
+                                             stderr_file, rfiles)
+            stdout_file = "%s-gffread_extract_transcripts-log_stdout.txt" % (run_id)
+            log_stdout = run.add_output_file("log_stdout",
+                                             stdout_file, rfiles)
 
-                run.add_output_file("fasta",
-                                    self.get_option('output-fasta-name'), fasta_file)
+            run.add_output_file("fasta",
+                                self.get_option('output-fasta-name'), rfiles)
 
-                exec_group = run.new_exec_group()
-                exec_group.add_command(cmd,
-                                       stdout_path=log_stdout,
-                                       stderr_path=log_stderr)
+            exec_group = run.new_exec_group()
+            exec_group.add_command(cmd,
+                                   stdout_path=log_stdout,
+                                   stderr_path=log_stderr)
