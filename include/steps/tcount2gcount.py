07b77168a857a120b18aa7742be46832c71df807
tiennes <sven-holger.puppel@izi.fraunhofer.de>
2018-05-14 12:46:35 +0200

    tools-tcount2gcount nwo accepts gtf and gives extended kallisto

--- include/steps/tcount2gcount.py
+++ include/steps/tcount2gcount.py
@@ -13,24 +13,31 @@ class Tcount2gcount(AbstractStep):
         super(Tcount2gcount, self).__init__(pipeline)
 
         # in connections
-        self.add_connection('in/transcript_counts')
+        self.add_connection('in/counts')
+        self.add_connection('in/annotation')
 
         # out connections
-        self.add_connection('out/gene_counts')
+        self.add_connection('out/counts')
 
         # options
         self.add_option('cores', int, optional=True, default=1,
                         description="workaround to specify cores for grid \
                                            engine and threads ie")
 
-        self.add_option('t', str, optional=False,
+        self.add_option('t', str, optional=True,
                         description='source tool of input file. ' \
                                     'Possible values: kallisto, salmon')
 
-        self.add_option('m', str, optional=False,
+        self.add_option('m', str, optional=True,
                         description='transcript to gene mapping file. ' \
                         'Required Format example (per row): ' \
-                        'ENST00000527779.1\tENSG00000137692.11')
+                        'ENST00000527779.1\tENSG00000137692.11' \
+                        ' or gtf')
+
+
+        self.add_option('kallisto-extended', bool, optional=True,
+                        description='writes extended format includign tpm. ' )
+
 
         # required tools
         self.require_tool('tcount2gcount')
@@ -38,24 +45,41 @@ class Tcount2gcount(AbstractStep):
     def runs(self, run_ids_connections_files):
         self.set_cores(self.get_option('cores'))
 
-        t_counts = []
-        for run_id in run_ids_connections_files.keys():
-            with self.declare_run(run_id) as run:
-                t_counts.append(run_ids_connections_files[run_id]['in/transcript_counts'][0])
 
-        for i in range(0, len(t_counts)):
-            new_run_id = 'tcount2gcount_%s' % i
-            with self.declare_run(new_run_id) as run:
+        annotation = None
+        for run_id in run_ids_connections_files.keys():
+            if 'in/annotation' in run_ids_connections_files[run_id]:
+                annotation = run_ids_connections_files[run_id]['in/annotation'][0]
+
+        for run_id in run_ids_connections_files.keys():
+            if 'in/annotation' in run_ids_connections_files[run_id]:
+                continue 
+
+            with self.declare_run(run_id) as run:
+                counts = run_ids_connections_files[run_id]['in/counts'][0]
+
                 tool_name = self.get_option('t')
-                file_name = '%s_%s.tsv' % (new_run_id, tool_name)
-                run.add_output_file('gene_counts', file_name, t_counts)
+                file_name = '%s-gene-abundance.tsv' % (run_id)
+                run.add_output_file('counts', file_name, [counts])
                 basename = run.get_output_directory_du_jour_placeholder()
-                convert_command = [
-                    self.get_tool('tcount2gcount'),
-                    '-m', self.get_option('m'),
-                    '-i', t_counts[i],
-                    '-t', tool_name,
-                    '-o', basename + '/' + file_name
-                ]
+
+                cmd = [self.get_tool('tcount2gcount')]
+
+                if self.is_option_set_in_config('m'): 
+                     cmd.extend(['-m', self.get_option('m')]) 
+                else:                    
+                    if annotation:
+                        cmd.extend(['-m', annotation]) 
+                    else:  
+                        logger.error("%s no annotation give via config or connection" % run_id) 
+                        sys.exit(1)
+                    
+                if self.is_option_set_in_config('kallisto-extended'): 
+                    cmd.append('--kallisto-extended')
+
+                cmd.extend(['-i', counts,
+                            '-t', tool_name,
+                            '-o', basename + '/' + file_name])
+
                 convert_exec_group = run.new_exec_group()
-                convert_exec_group.add_command(convert_command)
+                convert_exec_group.add_command(cmd)
