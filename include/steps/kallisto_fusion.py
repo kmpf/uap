9844f9cdc3648755f347a2b94a02b330d5fa6222
Schimmelpfennig <c.schimmelpfennig@ribnode011.izi.fraunhofer.de>
2018-05-23 16:21:11 +0200

    more options in kallisto_fusion

--- include/steps/kallisto_fusion.py
+++ include/steps/kallisto_fusion.py
@@ -18,6 +18,7 @@ class Kallisto(AbstractStep):
         # input connections
         self.add_connection('in/first_read')
         self.add_connection('in/second_read')
+        self.add_connection('in/kallisto-index')
 
         # output connections
         self.add_connection('out/fusion.txt')
@@ -31,7 +32,7 @@ class Kallisto(AbstractStep):
         self.require_tool('kallisto')
 
         # options
-        self.add_option('cores', int, optional=True, default=6,
+        self.add_option('cores', int, optional=True, default=1,
                         description="workaround to specify cores for grid \
                         engine and threads ie")
 
@@ -39,12 +40,51 @@ class Kallisto(AbstractStep):
                         description="Filename for the kallisto index to be \
                         used for quantification")
 
+        self.add_option('bias', bool, optional=True, default=None,
+                        description="Perform sequence based bias correction")
+
+        self.add_option('bootstrap-samples', int, optional=True, default=None,
+                        description="Number of bootstrap samples (default: 0)")
+
+        self.add_option('seed', int, optional=True, default=None,
+                        description="Seed for the bootstrap sampling") 
+        
+        self.add_option('single', int, optional=True, default=None,
+                        description="Quantify single-end reads")
+
+        self.add_option('single-overhang', int, optional=True, default=None,
+                        description="""Include reads where unobserved
+                        rest of fragment is predicted to lie
+                        outside a transcript""")
+
+        self.add_option('fr-stranded', bool, optional=True, default=None,
+                        description="Strand specific reads, first read forward")
+
+        self.add_option('rf-stranded', bool, optional=True, default=None,
+                        description="Strand specific reads, first read reverse")
+        
+        self.add_option('fragment-length', int, optional=True, default=None,
+                        description="Estimated average fragment length")
+        
+        self.add_option('sd', int, optional=True, default=None,
+                        description="Estimated standard deviation of fragment length")
+
     def runs(self, run_ids_connections_files):
         self.set_cores(self.get_option('cores'))
 
+        connect_index_path = None
         for run_id in run_ids_connections_files.keys():
-            with self.declare_run(run_id) as run:
+            try:
+                connect_index_path = run_ids_connections_files[run_id]['in/kallisto-index'][0]
+            except KeyError:
+                pass
 
+        for run_id in run_ids_connections_files.keys():
+            if 'in/kallisto-index' in run_ids_connections_files[run_id]:
+                continue
+
+            with self.declare_run(run_id) as run:
+                input_path = []
                 # Get lst of files for first/second read
                 fr_input = run_ids_connections_files[run_id]['in/first_read'][0]
                 sr_input = run_ids_connections_files[run_id]['in/second_read'][0]
@@ -87,12 +127,36 @@ class Kallisto(AbstractStep):
 
                     kallisto = [self.get_tool('kallisto'),
                                 'quant',
-                                '-i', self.get_option('index'),
-                                '--fusion',
-                                '-o', my_output,
-                                '--fr-stranded',
-                                fr_input,
-                                sr_input]
+                                '-t', str(self.get_option('cores')),
+                                '--fusion']
+                    if self.is_option_set_in_config('index'):
+                            kallisto.extend(['-i', self.get_option('index')])
+                    else:
+                        if connect_index_path:
+                            kallisto.extend(['-i', connect_index_path])
+                        else:
+                            logger.error(
+                            "%s no kallisto index give via config or connection" % run_id)
+                            exit(1)
+
+                    optns = ['fr-stranded', 'rf-stranded', 
+                            'bias', 'single-overhang', 'single'] 
+
+                    for optn in optns:
+                        if self.is_option_set_in_config(optn) and self.get_option(optn):
+                            kallisto.append('--' + flag)
+
+                    param_optns = ['bootstrap-samples', 'seed',
+                                'fragment-length', 'sd']
+                                
+                    for param_optn in param_optns:
+                        if self.is_option_set_in_config(param_optn):
+                            kallisto.extend(['--' + param_optn,
+                                str(self.get_option(param_optn))])
+                    
+                    kallisto.extend(['-o', my_output,
+                                    fr_input,
+                                    sr_input])
 
                     exec_group.add_command(kallisto,
                                            stdout_path=log_stdout,
