b08e6b05afeee06b765314502c76ecfebdd809f6
Christoph Kämpf <christoph.kaempf@izi.fraunhofer.de>
2018-10-25 15:17:46 +0200

    Change to __init__.py

--- include/subcommands/__init__.py
+++ include/subcommands/__init__.py
@@ -7,5 +7,5 @@ import steps
 import submit_to_cluster
 import run_info
 import volatilize
-__all__ = ['fix_problems', 'render', 'report', 'run_locally',
-           'status', 'steps', 'submit_to_cluster', 'run_info', 'volatilize']
+__all__ = ['fix_problems', 'render', 'report', 'run_locally', 'status', 'steps',
+           'submit_to_cluster', 'run_info', 'volatilize']
