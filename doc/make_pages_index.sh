cabe1655f6c62f199f1238a510f7ebaf1654d51b
Dominik <dominik.otto@izi.fraunhofer.de>
2020-01-20 13:15:28 +0100

    use pages index page to list branches (#117)

---
+++ doc/make_pages_index.sh
@@ -0,0 +1,21 @@
+#! /bin/bash -
+
+cd "$1"
+
+printf '
+<html>
+<body>
+<h2>UAP Sphinx Docu per branch</h2>
+<p>
+' > index.html
+
+for file in $(ls -1); do
+    [[ "$file" == "index.html" ]] && continue
+    printf '<li><a href=%s>%s</a></li>' "$file" "$file" >> index.html
+done
+
+printf '
+</p>
+</body>
+</html>
+' >> index.html
