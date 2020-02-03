9c2e198e17d592c97bf6ed3a30fe5969831ef225
Dominik <dominik.otto@izi.fraunhofer.de>
2020-02-03 15:51:01 +0100

    single end sensitive raw_file_sources

--- include/sources/run_folder_source.py
+++ include/sources/run_folder_source.py
@@ -23,7 +23,7 @@ class RunFolderSource(AbstractSourceStep):
         super(RunFolderSource, self).__init__(pipeline)
 
         self.add_connection('out/first_read')
-        self.add_connection('out/second_read')
+        self.add_connection('out/second_read', optional=True)
 
         self.add_option('path', str, optional=False,
                         description='Path to the sequencing directories that contain '
@@ -45,7 +45,7 @@ class RunFolderSource(AbstractSourceStep):
                         description = "Part of the file name that marks all "
                         "files containing sequencing data of the first read. "
                         "Example: '_R1.fastq' or '_1.fastq'")
-        self.add_option('second_read', str, default = "_R2",
+        self.add_option('second_read', str, default = "_R2", optional=True,
                         description = "Part of the file name that marks all "
                         "files containing sequencing data of the second read. "
                         "Example: 'R2.fastq.gz' or '_2.fastq'")
@@ -55,10 +55,15 @@ class RunFolderSource(AbstractSourceStep):
     def runs(self, run_ids_connections_files):
 
         found_samples = dict()
-        read_types = dict()
-        read_types['first_read'] = self.get_option('first_read')
+        read_types = {'first_read':self.get_option('first_read')}
 
-        if self.get_option('paired_end'):
+        paired_end = self.is_option_set_in_config('second_read')
+        if self.is_option_set_in_config('paired_end'):
+            if paired_end and not self.get_option('paired_end'):
+                raise UAPError('Second read passed but paired_end set to False.')
+            elif not paired_end and self.get_option('paired_end'):
+                raise UAPError('No second read passed but paired_end set to True.')
+        if paired_end:
             read_types['second_read'] = self.get_option('second_read')
 
         # let's look if the path to our data exists
@@ -96,7 +101,7 @@ class RunFolderSource(AbstractSourceStep):
         # create a run for each sample in found_samples and store data therein
         for run_id in found_samples.keys():
             with self.declare_run(run_id) as run:
-                run.add_public_info('paired_end', self.get_option('paired_end'))
+                run.add_public_info('paired_end', paired_end)
 
                 sample_path = None
 
@@ -105,9 +110,6 @@ class RunFolderSource(AbstractSourceStep):
                         for path in found_samples[run_id][read_types[read]]:
                             run.add_output_file(read, path, [])
                             sample_path = os.path.dirname(path)
-                    # always set the out connection even for zero files
-                    else:
-                        run.add_empty_output_connection(read)
 
                 # read sample sheets
                 sample_sheet_path = os.path.join(sample_path, 'SampleSheet.csv')
