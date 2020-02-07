93d00be28f91bea0861224b63a34d7e181fd76af
Dominik <dominik.otto@izi.fraunhofer.de>
2020-02-07 18:44:34 +0100

    empty connection deprication warning

--- include/run.py
+++ include/run.py
@@ -712,6 +712,9 @@ class Run(object):
         An empty output connection has 'None' as output file and 'None' as input
         file.
         '''
+        logger.warn('[Deprecation] add_empty_output_connection is depricated. '
+                'Please make the connection "out/%s" optional and do not add '
+                'anything instead.' % tag)
         # make sure tag was declared with an outgoing connection
         if 'out/' + tag not in self._step.get_out_connections():
             raise UAPError("Invalid output_file tag '%s' in %s. "
