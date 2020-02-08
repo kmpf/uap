5fe46debaecdddd90495bcc5ab6b2b3b11f6f284
Dominik <dominik.otto@izi.fraunhofer.de>
2020-02-08 12:01:27 +0100

    keep last ping files in fix-problems

--- include/pipeline.py
+++ include/pipeline.py
@@ -703,7 +703,7 @@ class Pipeline(object):
         for task in self.all_tasks_topologically_sorted:
             exec_ping_file = task.get_run().get_executing_ping_file()
             queued_ping_file = task.get_run().get_queued_ping_file()
-            failed_queued_ping_file = queued_ping_file + '.last'
+            last_queued_ping_file = queued_ping_file + '.last'
             if os.path.exists(exec_ping_file):
                 info = yaml.load(open(exec_ping_file, 'r'), Loader=yaml.FullLoader)
                 start_time = info['start_time']
@@ -720,12 +720,12 @@ class Pipeline(object):
                 info = yaml.load(open(queued_ping_file, 'r'), Loader=yaml.FullLoader)
                 if not str(info['job_id']) in running_jids:
                     queue_problems.append((task, queued_ping_file,
-                                           info['submit_time']))
-            if os.path.exists(failed_queued_ping_file):
-                info = yaml.load(open(failed_queued_ping_file, 'r'), Loader=yaml.FullLoader)
+                                           info['submit_time'], info['job_id']))
+            if os.path.exists(last_queued_ping_file):
+                info = yaml.load(open(last_queued_ping_file, 'r'), Loader=yaml.FullLoader)
                 if not str(info['job_id']) in running_jids:
-                    failed_problems.append((task, failed_queued_ping_file,
-                                           info['submit_time']))
+                    last_problems.append((task, last_queued_ping_file,
+                                           info['submit_time'], info['job_id']))
 
         show_hint = False
 
@@ -757,27 +757,28 @@ class Pipeline(object):
                     task = problem[0]
                     path = problem[1]
                     start_time = problem[2]
-                    print("submitted at %13s: %s" % (start_time, task))
+                    job_id = problem[3]
+                    print("submitted job %s at %13s: %s" % (job_id, start_time, task))
                 print("")
 
-        if len(failed_problems) > 0:
+        if len(last_problems) > 0:
             show_hint = True
-            label = "Warning: %d tasks were queued and failed to execute." % len(failed_problems)
+            label = "%d tasks were queued and stopped." % len(last_problems)
             print(label)
             if print_details:
                 print('-' * len(label))
-                failed_problems = sorted(failed_problems, key=itemgetter(2), reverse=True)
-                for problem in failed_problems:
+                last_problems = sorted(last_problems, key=itemgetter(2), reverse=True)
+                for problem in last_problems:
                     task = problem[0]
                     path = problem[1]
                     start_time = problem[2]
-                    print("submitted at %13s: %s" % (start_time, task))
+                    job_id = problem[3]
+                    print("submitted job %s at %13s: %s" % (job_id, start_time, task))
                 print("")
 
         if fix_problems:
             all_problems = run_problems
             all_problems.extend(queue_problems)
-            all_problems.extend(failed_problems)
             for problem in all_problems:
                 path = problem[1]
                 print("Now deleting %s..." % path)
