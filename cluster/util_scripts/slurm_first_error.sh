563915a52fb8a1e729c737328319deb207623095
Dominik <dominik.otto@izi.fraunhofer.de>
2020-02-08 20:25:01 +0100

    less output from --first-error

--- cluster/util_scripts/slurm_first_error.sh
+++ cluster/util_scripts/slurm_first_error.sh
@@ -7,14 +7,14 @@ shopt -s lastpipe
 
 for jobid in "$@"; do
     scontrol show job -o $jobid
-done | grep -v "JobState=COMPLETED" |
+done | grep -v "JobState=COMPLETED" | grep -v "JobState=RUNNING" |
     sed -E 's/^JobId=([0-9]*) .*EndTime=([^ ]*) .*/\2 \1/g' |
     sort -u | head -n1 | sed 's/[^ ]* //' |
     read stderrJob
 
 
 if [[ -z "$stderrJob" ]]; then
-    >&2 printf 'All found jobs were "COMPLETED".\n'
+    >&2 printf 'All found jobs were "RUNNING" or "COMPLETED".\n'
 else
     printf 'The first job that did not "COMPLETED" was %s.\n' "$stderrJob"
     cmd="scontrol show job $stderrJob"
@@ -25,6 +25,6 @@ else
         sed "s/\([^ ,]*\)=/\x0${esc}[34m\1: ${esc}[0m/g"
     stderrFile="$(eval "$cmd -o" | head -n1 |
         sed -E 's/.* StdErr=(.*) StdIn=.*/\1/')"
-    printf '\e[31mContent of: %s\e[0m\n' "$stderrFile"
-    cat "$stderrFile"
+    printf '\e[31mLast 30 lines of: %s\e[0m\n' "$stderrFile"
+    tail -n30 "$stderrFile"
 fi
