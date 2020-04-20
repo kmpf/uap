0eb62a82f913527efe72958b01c3da1e143ae731
Christoph Kämpf <christoph.kaempf@izi.fraunhofer.de>
2020-04-20 18:01:49 +0200

    Use abspath for cufflinks file options

--- include/steps/cufflinks.py
+++ include/steps/cufflinks.py
@@ -371,6 +371,10 @@ class CuffLinks(AbstractStep):
             if isinstance(self.get_option(option), bool):
                 if self.get_option(option):
                     option_list.append('--%s' % option)
+            elif option in ['mask-file', 'frag-bias-correct']:
+                option_list.append(
+                    os.path.abspath(self.get_option(option))
+                )
             else:
                 option_list.append('--%s' % option)
                 option_list.append(str(self.get_option(option)))
